9b094b824eea360524ba215113c89092f7487686
guyn <guyov1@gmail.com>
2017-05-09 21:32:07 +0300

    things are now working

--- how_to_update.sh
+++ how_to_update.sh
@@ -1,2 +1,6 @@
+echo 'Creating content... (>> pelican content)'
+pelican content
+echo 'Updating master branch... (>> ghp-import output -b master)'
 ghp-import output -b master
+echo 'Pushing to github.io ... (>> git push origin master)'
 git push origin master
