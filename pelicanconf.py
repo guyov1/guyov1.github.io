ef6c5d59701d8340d30c49632e45cc0434eb536f
guyn <guyov1@gmail.com>
2017-05-13 10:55:45 +0300

    now ignores ipynb checkpoints

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,10 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-MARKUP = ('md', 'ipynb')
+IGNORE_FILES = ['.ipynb_checkpoints']
 
-PLUGIN_PATH = './plugins'
+MARKUP = ('md', 'ipynb')
+#PLUGIN_PATH = './plugins'
+#PLUGINS = ['ipynb.markup']
+PLUGIN_PATHS = ['./pelican-plugins', './plugins']
 PLUGINS = ['ipynb.markup']
